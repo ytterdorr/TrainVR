0f8f09800b3b8c57562ee33d0561b15c3755c864
Tore Haglund <ytterdorr@gmail.com>
2018-10-25 23:33:47 +0200

    removed out-commented code

--- index.vr.js
+++ index.vr.js
@@ -37,35 +37,6 @@ export default class TrainVR extends React.Component {
     return (
       <View>
         <Pano source={asset("BIG_Bratcevo_Equirectangular.jpg")} />
-        {/* <Model
-          source={{
-            obj: asset("rails_formats.obj")
-            //mtl: asset("Rails_OBJ.obj")
-          }}
-          style={{
-            color: "black",
-            transform: [
-              { translate: [-5, -1.5, -5] },
-              { scale: 0.01 },
-              { rotateY: 90 }
-            ]
-          }}
-          wireframe={true}
-        /> */}
-        {/* <Model
-          source={{
-            obj: asset("Rails_OBJ.obj")
-          }}
-          style={{
-            color: "black",
-            transform: [
-              { translate: [-2, -1.5, -5] },
-              { scale: [0.0017, 0.001, 0.001] },
-              { rotateX: 270 }
-            ]
-          }}
-          wireframe={false}
-        /> */}
         <LeftTrack />
         <Track
           translate={[-20, -1.5, -6]}
